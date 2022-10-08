06304b309ad5c2ce536d08015828acbe7205b420
diegopalones <diego.palones@gmail.com>
2022-10-08 18:28:40 +0200

    introducidas validaciones

--- main.js
+++ main.js
@@ -13,3 +13,33 @@ let usuarios = [];
 let listarUsuario = document.querySelector("#lista");
 let mensajeAlerta = document.querySelector("#mensajeAlerta");
 let mensajeAlertaVerde = document.querySelector("#mensajeAlertaVerde");
+
+// Validaciones
+
+function validar() {
+    let isValid = false;
+    if (
+      nombre.value === "" ||
+      email.value === "" ||
+      password.value === "" ||
+      passConfirm.value === ""
+    ) {
+      mensajeAlerta.style.opacity = 1;
+      printMsg("Por favor, rellena todos los campos.");
+    } else if (validationEmail.test(email.value) !== true) {
+      printMsg("Por favor, introduce un email válido.");
+      email.value = "";
+      mensajeAlerta.style.opacity = 1;
+    } else if (password.value !== passConfirm.value) {
+      printMsg("Las contraseñas tienen que coincidir");
+      mensajeAlerta.style.opacity = 1;
+      password.value = "";
+      passConfirm.value = "";
+    } else {
+      msg.style.color = "#fff";
+      printMsg("Usuario creado correctamente!");
+      mensajeAlertaVerde.style.opacity = 1;
+      isValid = true;
+    }
+    return isValid;
+  }
