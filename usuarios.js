c61b556d103ecb66445700ce65f68961a755c68a
sergiokano <113986570+sergiokano@users.noreply.github.com>
2022-10-10 08:49:55 +0200

    Usuarios actualizado

--- usuarios.js
+++ usuarios.js
@@ -6,10 +6,6 @@
 
 // function cardsData() {
 // let users = JSON.parse(localStorage.getItem("usuarios"));
-// function getRandom(array) {
-//     return array[Math.floor(Math.random() * array.length)]          //Creamos un método para que las imágenes aparezcan al azar
-// }
-// const imgRandom = ["Assets/RANDOM/camello.jpg", "Assets/RANDOM/conejo.jpg", "Assets/RANDOM/mono.jpg", "Assets/RANDOM/perrito.jpg", "Assets/RANDOM/perro.jpg", "Assets/RANDOM/gatito.jpg"];                            //Array de las imágenes
 
 // }
 // for (const user of users) {
